9cd2e5e6499a9c08ce820da77a7b7fcc0862c812
lars <55391234+lpjc@users.noreply.github.com>
2023-09-08 14:55:46 +0200

    two options

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios'; // Import Axios
 import './App.css';
 
+function ThemeOption({ value, name }) {
+  return <option value={value}>{name}</option>;
+}
+
 function App() {
   const [legoSets, setLegoSets] = useState([]);
   const apiKey = 'e00fe84219678222906b99be611d12c1'; // Replace with your actual API key
+  const [theme, setTheme] = useState(171); // Default theme ID (Star Wars)
+  const [filteredSets, setFilteredSets] = useState([]); // Declare filteredSets state variable
 
   useEffect(() => {
     // Define the Rebrickable API endpoint
-    const apiUrl = 'https://rebrickable.com/api/v3/lego/sets/?page=1&page_size15';
+    const apiUrl = 'https://rebrickable.com/api/v3/lego/sets/?page_size=1000';
 
     // Define headers with your API key
     const headers = {
@@ -34,25 +40,36 @@ function App() {
       });
   }, []); // The empty dependency array ensures this effect runs only once
 
-/* 
-Possible data from API:
-legoSet.set_num
-legoSet.theme_id
+  useEffect(() => {
+    // Define a filtering function to include sets with more than 50 pieces and the selected theme
+    const filterSets = (legoSet) => {
+      return legoSet.num_parts > 50 && legoSet.theme_id === theme;
+    };
 
-*/
-
+    const filteredSets = legoSets.filter(filterSets);
+    setFilteredSets(filteredSets); // Update filtered sets whenever the theme changes
+  }, [theme, legoSets]);
 
 
   return (
     <div>
       <h1>LEGO Sets</h1>
+      <div>
+        <label>Select Theme:</label>
+        <select onChange={(e) => setTheme(parseInt(e.target.value))} value={theme}>
+          <ThemeOption value={171} name="Star Wars" />
+          <ThemeOption value={246} name="Harry Potter" />
+          {/* Add more theme options as needed */}
+        </select>
+      </div>
       <div className="lego-set-container">
-        {legoSets.map((legoSet) => (
+        {filteredSets.map((legoSet) => (
           <div className="lego-set-card" key={legoSet.set_num}>
             <h2>{legoSet.name}</h2>
             <p>Set Number: {legoSet.set_num}</p>
             <p>Theme: {legoSet.theme_id}</p>
             <p>Year: {legoSet.year}</p>
+            <p>Number of Parts: {legoSet.num_parts}</p>
             <img src={legoSet.set_img_url} alt={`Image of ${legoSet.name}`} className="lego-set-image" />
             {/* Add more details as needed */}
           </div>
