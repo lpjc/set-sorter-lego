e3070b0460a990f5766a3eeb2ea019edd5c2ca7a
lars <55391234+lpjc@users.noreply.github.com>
2023-09-09 23:03:43 +0200

    Layout and shuffle functions

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
   const apiKey = 'e00fe84219678222906b99be611d12c1';
   const [theme, setTheme] = useState(171);
   const [filteredSets, setFilteredSets] = useState([]);
-  const [currentSetIndex, setCurrentSetIndex] = useState(0);
+  const [currentSetIndex, setCurrentSetIndex] = useState(null); // Initialize as null
   const [guess, setGuess] = useState('');
+  const [feedback, setFeedback] = useState('');
   const [isCorrect, setIsCorrect] = useState(false);
-  const [offBy, setOffBy] = useState(null); // Track how many parts the guess is off by
 
   useEffect(() => {
     const apiUrl = 'https://rebrickable.com/api/v3/lego/sets/?page_size=1000';
@@ -32,73 +32,110 @@ function App() {
   useEffect(() => {
     const filteredSets = legoSets.filter((legoSet) => legoSet.theme_id === theme);
     setFilteredSets(filteredSets);
-    setCurrentSetIndex(0);
+
+    // Select a random set from filteredSets on load
+    const randomIndex = Math.floor(Math.random() * filteredSets.length);
+    setCurrentSetIndex(randomIndex);
+
     setGuess('');
+    setFeedback('');
     setIsCorrect(false);
-    setOffBy(null); // Reset offBy when theme changes
   }, [theme, legoSets]);
 
   const handleGuessSubmit = (e) => {
     e.preventDefault();
-
     const currentSet = filteredSets[currentSetIndex];
     const userGuess = parseInt(guess);
 
-    if (currentSet && userGuess === currentSet.num_parts) {
+    if (userGuess === currentSet.num_parts) {
       setIsCorrect(true);
-      setOffBy(null); // Reset offBy when the guess is correct
-    } else {
+      setFeedback('Correct!');
+    } else if (userGuess < currentSet.num_parts) {
+      setFeedback('Higher');
       setIsCorrect(false);
-      setOffBy(currentSet.num_parts - userGuess);
+    } else {
+      setFeedback('Lower');
+      setIsCorrect(false);
     }
   };
 
   const handleNextSet = () => {
-    setCurrentSetIndex((prevIndex) => prevIndex + 1);
-    setGuess('');
-    setIsCorrect(false);
-    setOffBy(null); // Reset offBy when moving to the next set
+    if (filteredSets.length > 0) {
+      const shuffleDuration = 800; // Duration of the shuffle in milliseconds
+      const frameDelay = 40; // Delay between frames in milliseconds
+      let startTime = null;
+      let currentIndex = currentSetIndex;
+  
+      const animateShuffle = (timestamp) => {
+        if (!startTime) {
+          startTime = timestamp;
+        }
+  
+        const elapsedTime = timestamp - startTime;
+  
+        if (elapsedTime < shuffleDuration) {
+          // Shuffle images
+          let newIndex;
+          do {
+            newIndex = Math.floor(Math.random() * filteredSets.length);
+          } while (newIndex === currentIndex);
+  
+          currentIndex = newIndex; // Update currentIndex here
+  
+          setCurrentSetIndex(newIndex);
+  
+          // Request the next animation frame with the specified delay
+          setTimeout(() => {
+            requestAnimationFrame(animateShuffle);
+          }, frameDelay);
+        } else {
+          // After the shuffle, set the final image
+          setCurrentSetIndex(currentIndex);
+  
+          // Reset other values
+          setGuess('');
+          setFeedback('');
+          setIsCorrect(false);
+        }
+      };
+  
+      // Start the shuffle animation
+      requestAnimationFrame(animateShuffle);
+    }
   };
+  
 
   return (
     <div className="container">
-      <h1>LEGO Set Guessing Game</h1>
-      <div>
-        <label>Select Theme:</label>
-        <select onChange={(e) => setTheme(parseInt(e.target.value))} value={theme}>
-          <option value={171}>Star Wars</option>
-          <option value={246}>Harry Potter</option>
-          {/* Add more theme options as needed */}
-        </select>
-      </div>
-      {filteredSets.length > 0 && currentSetIndex < filteredSets.length && (
-        <div className="lego-set-card">
-          <h2>{filteredSets[currentSetIndex].name}</h2>
-          <img src={filteredSets[currentSetIndex].set_img_url} alt={`Image of LEGO Set`} className="lego-set-image" />
-          <form onSubmit={handleGuessSubmit}>
-            <label>Enter your guess:</label>
-            <input
-              type=""
-              value={guess}
-              onChange={(e) => setGuess(e.target.value)}
-              disabled={isCorrect}
-            />
-            <button type="submit" disabled={isCorrect}>
-              Submit Guess
+      {currentSetIndex !== null && filteredSets.length > 0 && (
+        <div className="card-container">
+          <div className="image-card">
+            <img src={filteredSets[currentSetIndex].set_img_url} alt={`Image of LEGO Set`} />
+          </div>
+          <div className="input-card">
+            <h2>{filteredSets[currentSetIndex].name}</h2>
+            <form onSubmit={handleGuessSubmit}>
+              <input
+                type="number"
+                value={guess}
+                onChange={(e) => setGuess(e.target.value)}
+                disabled={isCorrect}
+              />
+              <button type="submit" disabled={isCorrect}>
+                Guess
+              </button>
+            </form>
+            <div className="feedback">
+              {isCorrect ? (
+                <p className="correct">{feedback}</p>
+              ) : (
+                <p className="off-by">{feedback}</p>
+              )}
+            </div>
+            <button className="next-button" onClick={handleNextSet}>
+              Next
             </button>
-          </form>
-          {isCorrect ? (
-            <p className="correct">Correct! The set has {filteredSets[currentSetIndex].num_parts} parts.</p>
-          ) : (
-            <p className="off-by">
-              {offBy !== null && offBy !== 0
-                ? `You are off by ${Math.abs(offBy)} part(s).`
-                : offBy === 0
-                ? 'You got it exactly right!'
-                : 'Make a guess!'}
-            </p>
-          )}
-          <button onClick={handleNextSet}>Next Set</button>
+          </div>
         </div>
       )}
     </div>
