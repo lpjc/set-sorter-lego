71d5528239fc571cd04bef6ad3dbda3732bf591b
lars <55391234+lpjc@users.noreply.github.com>
2023-09-09 22:34:25 +0200

    Functioning part guesser

--- src/App.js
+++ src/App.js
@@ -1,85 +1,106 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'; // Import Axios
+import axios from 'axios';
 import './App.css';
-
-function ThemeOption({ value, name }) {
-  return <option value={value}>{name}</option>;
-}
 
 function App() {
   const [legoSets, setLegoSets] = useState([]);
-  const apiKey = 'e00fe84219678222906b99be611d12c1'; // Replace with your actual API key
-  const [theme, setTheme] = useState(171); // Default theme ID (Star Wars)
-  const [filteredSets, setFilteredSets] = useState([]); // Declare filteredSets state variable
+  const apiKey = 'e00fe84219678222906b99be611d12c1';
+  const [theme, setTheme] = useState(171);
+  const [filteredSets, setFilteredSets] = useState([]);
+  const [currentSetIndex, setCurrentSetIndex] = useState(0);
+  const [guess, setGuess] = useState('');
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [offBy, setOffBy] = useState(null); // Track how many parts the guess is off by
 
   useEffect(() => {
-    // Define the Rebrickable API endpoint
     const apiUrl = 'https://rebrickable.com/api/v3/lego/sets/?page_size=1000';
-
-    // Define headers with your API key
     const headers = {
       Accept: 'application/json',
       Authorization: `key ${apiKey}`,
     };
 
-    // Make a GET request to fetch LEGO set information
-    axios.get(apiUrl, { headers })
+    axios
+      .get(apiUrl, { headers })
       .then((response) => {
-        // Update the state with the fetched data
-        console.log('API response:', response);
         setLegoSets(response.data.results);
-
-        // Log API connection status
-        console.log('API connection successful');
       })
       .catch((error) => {
         console.error('Error fetching LEGO set information:', error);
-
-        // Log API connection status
-        console.log('API connection failed');
       });
-  }, []); // The empty dependency array ensures this effect runs only once
-
-
-  const themeArray = [1, 2, 3]
-
+  }, []);
 
   useEffect(() => {
-    // Define a filtering function to include sets with more than 50 pieces and the selected theme
-    const filterSets = (legoSet) => {
-      return legoSet.num_parts > 50 && legoSet.theme_id === theme;
-    };
-
-    const filteredSets = legoSets.filter(filterSets);
-    setFilteredSets(filteredSets); // Update filtered sets whenever the theme changes
-    console.log("fitlered set: " + filteredSets.length)
+    const filteredSets = legoSets.filter((legoSet) => legoSet.theme_id === theme);
+    setFilteredSets(filteredSets);
+    setCurrentSetIndex(0);
+    setGuess('');
+    setIsCorrect(false);
+    setOffBy(null); // Reset offBy when theme changes
   }, [theme, legoSets]);
 
+  const handleGuessSubmit = (e) => {
+    e.preventDefault();
+
+    const currentSet = filteredSets[currentSetIndex];
+    const userGuess = parseInt(guess);
+
+    if (currentSet && userGuess === currentSet.num_parts) {
+      setIsCorrect(true);
+      setOffBy(null); // Reset offBy when the guess is correct
+    } else {
+      setIsCorrect(false);
+      setOffBy(currentSet.num_parts - userGuess);
+    }
+  };
+
+  const handleNextSet = () => {
+    setCurrentSetIndex((prevIndex) => prevIndex + 1);
+    setGuess('');
+    setIsCorrect(false);
+    setOffBy(null); // Reset offBy when moving to the next set
+  };
 
   return (
-    <div>
-      <h1>LEGO Sets</h1>
+    <div className="container">
+      <h1>LEGO Set Guessing Game</h1>
       <div>
         <label>Select Theme:</label>
         <select onChange={(e) => setTheme(parseInt(e.target.value))} value={theme}>
-          <ThemeOption value={171} name="Star Wars" />
-          <ThemeOption value={246} name="Harry Potter" />
+          <option value={171}>Star Wars</option>
+          <option value={246}>Harry Potter</option>
           {/* Add more theme options as needed */}
         </select>
       </div>
-      <div className="lego-set-container">
-        {filteredSets.map((legoSet) => (
-          <div className="lego-set-card" key={legoSet.set_num}>
-            <h2>{legoSet.name}</h2>
-            <p>Set Number: {legoSet.set_num}</p>
-            <p>Theme: {legoSet.theme_id}</p>
-            <p>Year: {legoSet.year}</p>
-            <p>Number of Parts: {legoSet.num_parts}</p>
-            <img src={legoSet.set_img_url} alt={`Image of ${legoSet.name}`} className="lego-set-image" />
-            {/* Add more details as needed */}
-          </div>
-        ))}
-      </div>
+      {filteredSets.length > 0 && currentSetIndex < filteredSets.length && (
+        <div className="lego-set-card">
+          <h2>{filteredSets[currentSetIndex].name}</h2>
+          <img src={filteredSets[currentSetIndex].set_img_url} alt={`Image of LEGO Set`} className="lego-set-image" />
+          <form onSubmit={handleGuessSubmit}>
+            <label>Enter your guess:</label>
+            <input
+              type=""
+              value={guess}
+              onChange={(e) => setGuess(e.target.value)}
+              disabled={isCorrect}
+            />
+            <button type="submit" disabled={isCorrect}>
+              Submit Guess
+            </button>
+          </form>
+          {isCorrect ? (
+            <p className="correct">Correct! The set has {filteredSets[currentSetIndex].num_parts} parts.</p>
+          ) : (
+            <p className="off-by">
+              {offBy !== null && offBy !== 0
+                ? `You are off by ${Math.abs(offBy)} part(s).`
+                : offBy === 0
+                ? 'You got it exactly right!'
+                : 'Make a guess!'}
+            </p>
+          )}
+          <button onClick={handleNextSet}>Next Set</button>
+        </div>
+      )}
     </div>
   );
 }
